5f2621ce6cf580b9c941137f46ce5a8adadec337
prasanthmohan786 <prasanthmohanbtech@outlook.in>
2020-07-18 23:20:34 +0530

    angular first commit

---
+++ WEBAPI/WEBAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBAPI.Models
+{        public class PaymentDetailContext : DbContext
+        {
+            public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+            { }
+
+            public DbSet<PaymentDetail> PaymentDetails { get; set; }
+        }    
+}
---
+++ WEBAPI/WEBAPI/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WEBAPI.Models;
+
+namespace WEBAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //remove default json formatting
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+            });
+
+            services.AddDbContext<PaymentDetailContext>(options =>
+           options.UseSqlServer("data source=DESKTOP-40GEPN6\\SQLEXPRESS;Database = PaymentDetailDB; Trusted_Connection = True; MultipleActiveResultSets = True;"));
+            //add cors package
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.WithOrigins("http://localhost:4200"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseCors(options => options.WithOrigins("http://localhost:4200"));
+        }
+    }
+}
